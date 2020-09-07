38f13bfbbab79d3b0700a985556a22c97209681f
Nick Cotter <nick@extropy.net>
2020-09-07 18:22:33 +0100

    reworked to load official uk case data

--- UKCovid19Explorer/app.R
+++ UKCovid19Explorer/app.R
@@ -2,19 +2,30 @@ library(shiny)
 library(tidyverse)
 library(leaflet)
 library(shinycssloaders)
+library(readr)
+library(lubridate)
+library(R0)
+library(zoo)
 
-# load static data
-englandNIData <- read.csv("https://extropy-datascience-public.s3-eu-west-1.amazonaws.com/data/ons/local-authorities/Local_Authority_Districts_(April_2019)_Boundaries_UK_BFE.csv") %>%
-    mutate(AreaCode = LAD19CD) %>%
-    mutate(Longitude = LONG) %>%
-    mutate(Latitude = LAT) %>%
-    select(c(AreaCode, Longitude, Latitude))
 
-walesData <- read.csv("https://extropy-datascience-public.s3-eu-west-1.amazonaws.com/data/ons/local-authorities/Wales_Postcodes-120520.csv") %>%
-    mutate(AreaCode=Primary.Care.Trust.Code) %>%
-    select(c(AreaCode, Longitude, Latitude))
+# load latest data
+raw_data <- read_csv("https://coronavirus.data.gov.uk/downloads/csv/coronavirus-cases_latest.csv")
 
-combinedGeoData <- bind_rows(englandNIData, walesData)
+# pre-processing
+preProcessedData <- raw_data %>%
+    mutate(area = `Area name`) %>%
+    mutate(code = `Area code`) %>%
+    mutate(type = `Area type`) %>%
+    mutate(date = ymd(`Specimen date`)) %>%
+    mutate(cases = `Daily lab-confirmed cases`) %>%
+    arrange(date) %>%
+    dplyr::select(area, type, code, date, cases)
+
+# lower tier local authority data
+lowerTierData <- preProcessedData %>%
+    filter(type == 'ltla')
+
+areaNames <- unique(lowerTierData$area)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -22,46 +33,53 @@ ui <- fluidPage(
     # Application title
     titlePanel("UK COVID-19 Explorer"),
     
-    tabsetPanel(type = "tabs",
-                tabPanel("Map", leafletOutput("map", height = 800) %>% withSpinner(color="#0dc5c1", type=4)),
-                tabPanel("Top 10", plotOutput("top10") %>% withSpinner(color="#0dc5c1", type=4))
-    )
+    sidebarLayout(
+        
+        # Sidebar with a slider input
+        sidebarPanel(
+            selectInput("lowerTierAreaName", "Area", areaNames)
+        ),
+        
+        mainPanel(
+            plotOutput("areaCases") 
+        )
+        
+        #mainPanel(
+        #    tabsetPanel(type="tabs",
+#                        tabPanel("Area", plotOutput("areaCases")))    
+ #       )
+        
+    ),
+    
+
+    
+#    tabsetPanel(type = "tabs",
+#                tabPanel("Top 10", plotOutput("top10") %>% withSpinner(color="#0dc5c1", type=4))
+##                tabPanel("Map", leafletOutput("map", height = 800) %>% withSpinner(color="#0dc5c1", type=4)),
+#    )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
-    caseData <- reactive({
-        raw_data <- read.csv("https://raw.githubusercontent.com/tomwhite/covid-19-uk-data/master/data/covid-19-cases-uk.csv")
-        raw_data %>%
-            mutate(Date = as.Date(Date)) %>%
-            mutate(TotalCases = as.integer(TotalCases)) %>%
-            filter(TotalCases > 0)
+    localData <- reactive({
+        lowerTierData %>%
+            filter(area==input$lowerTierAreaName)
     })
     
-    latestData <- reactive({
-        caseData() %>%
-            group_by(AreaCode) %>% 
-            slice(which.max(Date))
+    output$areaCases <- renderPlot({
+        ggplot(localData()) + aes(x=date, y=cases) + ylim(0, NA) + geom_line() + geom_smooth(method = "loess")
     })
     
-    output$top10 <- renderPlot({
-        top10 <- latestData() %>% ungroup() %>% top_n(10, TotalCases)
-        ggplot(top10, aes(x=reorder(Area, TotalCases), y=TotalCases))  + geom_col() +
-            xlab("Area") +
-            ylab("Total Cases") +
-            coord_flip()
-    })
-    
-    output$map <- renderLeaflet({
-        mappedLatestData <- latestData() %>%
-            left_join(combinedGeoData)
-        
-        leaflet(mappedLatestData) %>%
-            addTiles() %>%
-            addCircles(lng = ~Longitude, lat = ~Latitude, weight = 1, radius = ~TotalCases * 10, popup = ~paste(Area, ":", TotalCases)) %>%
-            setView(lng = -2.89479, lat = 54.093409, zoom = 6)
-    })
+    #output$map <- renderLeaflet({
+    #    mappedLatestData <- latestData() %>%
+    #        left_join(combinedGeoData)
+    #    
+    #    leaflet(mappedLatestData) %>%
+    #        addTiles() %>%
+    #        addCircles(lng = ~Longitude, lat = ~Latitude, weight = 1, radius = ~TotalCases * 10, popup = ~paste(Area, ":", TotalCases)) %>%
+    #        setView(lng = -2.89479, lat = 54.093409, zoom = 6)
+    #})
 }
 
 # Run the application 
